24181c80474101fda0ca16658d2667a17bdfb7f6
Roman Chaban <romanchaban9@gmail.com>
2022-04-25 20:53:16 +0300

    made Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import classNames from 'classnames';
 
-export const Button = () => {
-
+export const Button = ({children, outline, cart}) => {
+    return (
+        <button className={classNames('button', {
+            'button-outline': outline,
+            'button--cart': cart,
+        })}>
+            {children}
+        </button>
+    )
 }
