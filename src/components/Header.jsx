f5d4b23cdc6ebab7d80dcd1ff681d63905728d49
Roman Chaban <romanchaban9@gmail.com>
2022-04-28 15:37:27 +0300

    fixed logo img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from '../assets/images/pizzaLogo.svg';
 import {Button} from "./Button";
 
 export const Header = () => {
@@ -6,7 +7,7 @@ export const Header = () => {
         <div className="header">
             <div className="container">
                 <div className="header__logo">
-                    <img width="38" src="./images/pizzaLogo.svg" alt="Pizza logo"/>
+                    <img width="38" src={logo} alt="Pizza logo"/>
                     <div>
                         <h1>React Pizza</h1>
                         <p>самая вкусная пицца во вселенной</p>
